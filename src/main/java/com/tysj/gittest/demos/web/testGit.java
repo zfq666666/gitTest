d38055e0240dbea8c16eb03936b9e87c96bbba7e
zfq <zfq18332698469@163.com>
2023-10-10 15:21:17 +0800

    second commit

---
+++ src/main/java/com/tysj/gittest/demos/web/testGit.java
@@ -0,0 +1,9 @@
+package com.tysj.gittest.demos.web;
+
+public class testGit {
+    public static void main(String[] args) {
+        System.out.println("master!!!!!");
+        System.out.println("master!!!!!");
+        System.out.println("hot-fix!!!!!");
+    }
+}
